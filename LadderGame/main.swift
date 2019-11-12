4cda9a04eff03df7658f74b07274a23112cb926c
Seonghun <kim.seonghun23@gmail.com>
2019-11-12 15:44:15 +0900

    코드 리팩토링

--- LadderGame/main.swift
+++ LadderGame/main.swift
@@ -7,34 +7,49 @@
 
 import Foundation
 
+typealias LadderPlayer = String
+
 struct SingleLadderGame {
-    struct LadderPlayer {
-        var name = ""
-    }
+    var input: UserInputType?
+    var output: outputType?
     
-    static func readHeight() -> Int {
+    mutating func run() {
+        guard let height = input?.getHeight() else { return }
+        guard let players = input?.getPlayerNames() else { return }
+        
+        output?.printLadders(height: height, players: players)
+    }
+}
+
+protocol UserInputType {
+    func getHeight() -> Int
+    func getPlayerNames() -> [String]
+}
+
+struct UserInput: UserInputType {
+    static let shared = UserInput()
+    
+    func getHeight() -> Int {
         print("사다리 높이를 입력해주세요.")
         let height = readLine() ?? ""
         return Int(height) ?? 0
     }
     
-    static func readPlayerNames() -> [String] {
+    func getPlayerNames() -> [String] {
         print("참여할 사람 이름을 입력하세요.")
         let players = readLine() ?? ""
         return players.split(separator: ",").map{String($0)}
     }
+}
+
+protocol outputType {
+    func printLadders(height: Int, players: [LadderPlayer])
+}
+
+struct Output: outputType {
+    static let shared = Output()
     
-    var height = 0
-    var players = [LadderPlayer]()
-    
-    mutating func run() {
-        self.height = SingleLadderGame.readHeight()
-        let names = SingleLadderGame.readPlayerNames()
-        self.players = names.map({LadderPlayer(name:$0)})
-        printLadders()
-    }
-    
-    func printLadders() {
+    func printLadders(height: Int, players: [LadderPlayer]) {
         for _ in 0..<height {
             print("|", terminator:"")
             for _ in 0..<players.count {
@@ -51,4 +66,7 @@ struct SingleLadderGame {
 }
 
 var game = SingleLadderGame()
+game.input = UserInput.shared
+game.output = Output.shared
+
 game.run()
