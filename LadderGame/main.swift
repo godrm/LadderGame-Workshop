e8c7fbbf34f86f0d1282f40524bb2f88637f9bdf
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-11-12 15:53:55 +0900

    리팩토링!

--- LadderGame/main.swift
+++ LadderGame/main.swift
@@ -7,41 +7,31 @@
 
 import Foundation
 
-struct SingleLadderGame {
-    struct LadderPlayer {
-        var name = ""
+protocol Game {
+    mutating func run()
+}
+
+/// IO task handler
+struct IOHandler {
+    static func readInt(message: String) -> Int {
+        print(message)
+        let value = readLine() ?? ""
+        return Int(value) ?? 0
     }
     
-    static func readHeight() -> Int {
-        print("사다리 높이를 입력해주세요.")
-        let height = readLine() ?? ""
-        return Int(height) ?? 0
+    static func readStrings(message: String) -> [String] {
+        print(message)
+        let value = readLine() ?? ""
+        return value.split(separator: ",").map { String($0) }
     }
     
-    static func readPlayerNames() -> [String] {
-        print("참여할 사람 이름을 입력하세요.")
-        let players = readLine() ?? ""
-        return players.split(separator: ",").map{String($0)}
-    }
-    
-    var height = 0
-    var players = [LadderPlayer]()
-    
-    mutating func run() {
-        self.height = SingleLadderGame.readHeight()
-        let names = SingleLadderGame.readPlayerNames()
-        self.players = names.map({LadderPlayer(name:$0)})
-        printLadders()
-    }
-    
-    func printLadders() {
-        for _ in 0..<height {
+    static func printLadders(height: Int, count: Int) {
+        for _ in 0 ..< height {
             print("|", terminator:"")
-            for _ in 0..<players.count {
-                if Int(arc4random_uniform(2))==1 {
+            for _ in 0 ..< count {
+                if Int(arc4random_uniform(2)) == 1 {
                     print("---", "|", separator:"", terminator:"")
-                }
-                else {
+                } else {
                     print("   ", "|", separator:"", terminator:"")
                 }
             }
@@ -50,5 +40,44 @@ struct SingleLadderGame {
     }
 }
 
-var game = SingleLadderGame()
+/// Player information
+struct LadderPlayer {
+    var name = ""
+}
+
+/// Game infromation
+struct GameInfo {
+    var height: Int = 0
+    var players: [LadderPlayer] = []
+}
+
+/// Game task manager
+struct SingleLadderGame: Game {
+    private var gameInfo: GameInfo
+    
+    init(info: GameInfo) {
+        gameInfo = info
+    }
+    
+    mutating func run() {
+        gameInfo.height = readHeight()
+        gameInfo.players = readPlayerNames()
+        printLadders()
+    }
+    
+    private func readHeight() -> Int {
+        IOHandler.readInt(message: "사다리 높이를 입력해주세요.")
+    }
+    
+    private func readPlayerNames() -> [LadderPlayer] {
+        IOHandler.readStrings(message: "참여할 사람 이름을 입력하세요.")
+            .map { LadderPlayer(name:$0) }
+    }
+    
+    private func printLadders() {
+        IOHandler.printLadders(height: gameInfo.height, count: gameInfo.players.count)
+    }
+}
+
+var game: Game = SingleLadderGame(info: GameInfo())
 game.run()
