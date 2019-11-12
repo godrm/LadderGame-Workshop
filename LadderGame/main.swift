1584d11eb2809787fa497e89236a3b183f47f5a2
Presto <yoohan95@gmail.com>
2019-11-12 15:43:28 +0900

    수정

--- LadderGame/main.swift
+++ LadderGame/main.swift
@@ -7,47 +7,95 @@
 
 import Foundation
 
-struct SingleLadderGame {
-    struct LadderPlayer {
-        var name = ""
+protocol SingleLadderGameType {
+
+  associatedtype Input: SingleLadderGameInputType
+
+  associatedtype Output: SingleLadderGameOutputType
+}
+
+protocol SingleLadderGameInputType {
+
+  func readHeight() -> Int
+
+  func readPlayerNames() -> [String]
+}
+
+protocol SingleLadderGameOutputType {
+
+  func printLadderGrid(_: [[String]])
+}
+
+struct SingleLadderGame: SingleLadderGameType {
+
+  struct Input: SingleLadderGameInputType {
+
+    func readHeight() -> Int {
+      print("사다리 높이를 입력해주세요.")
+      let height = readLine() ?? ""
+      return Int(height) ?? 0
     }
-    
-    static func readHeight() -> Int {
-        print("사다리 높이를 입력해주세요.")
-        let height = readLine() ?? ""
-        return Int(height) ?? 0
+
+    func readPlayerNames() -> [String] {
+      print("참여할 사람 이름을 입력하세요.")
+      let players = readLine() ?? ""
+      return players.split(separator: ",").map { String($0) }
     }
-    
-    static func readPlayerNames() -> [String] {
-        print("참여할 사람 이름을 입력하세요.")
-        let players = readLine() ?? ""
-        return players.split(separator: ",").map{String($0)}
-    }
-    
-    var height = 0
-    var players = [LadderPlayer]()
-    
-    mutating func run() {
-        self.height = SingleLadderGame.readHeight()
-        let names = SingleLadderGame.readPlayerNames()
-        self.players = names.map({LadderPlayer(name:$0)})
-        printLadders()
-    }
-    
-    func printLadders() {
-        for _ in 0..<height {
-            print("|", terminator:"")
-            for _ in 0..<players.count {
-                if Int(arc4random_uniform(2))==1 {
-                    print("---", "|", separator:"", terminator:"")
-                }
-                else {
-                    print("   ", "|", separator:"", terminator:"")
-                }
-            }
-            print()
+  }
+
+  struct Output: SingleLadderGameOutputType {
+
+    func printLadderGrid(_ grid: [[String]]) {
+      for row in grid {
+        for column in row {
+          print(column, separator: "", terminator: "")
         }
+      }
     }
+  }
+
+  struct Process {
+
+    func namesToLadderPlayers(_ names: [String]) -> [LadderPlayer] {
+      names.map(LadderPlayer.init)
+    }
+
+    func ladderGrid(forHeight height: Int, players: [LadderPlayer]) -> [[String]] {
+      var grid = [[String]]()
+      for _ in 0 ..< height {
+        var row = [String]()
+        row.append("|")
+        for _ in 0 ..< players.count {
+          if Int.random(in: 0 ..< 2) == 1 {
+            row.append(contentsOf: ["---", "|"])
+          } else {
+            row.append(contentsOf: ["   ", "|"])
+          }
+        }
+        row.append("\n")
+        grid.append(row)
+      }
+      return grid
+    }
+  }
+
+  struct LadderPlayer {
+
+    var name = ""
+  }
+
+  func run() {
+    let input = SingleLadderGame.Input()
+    let process = SingleLadderGame.Process()
+    let output = SingleLadderGame.Output()
+
+    let height = input.readHeight()
+    let names = input.readPlayerNames()
+    let players = process.namesToLadderPlayers(names)
+
+    let grid = process.ladderGrid(forHeight: height, players: players)
+    output.printLadderGrid(grid)
+  }
 }
 
 var game = SingleLadderGame()
