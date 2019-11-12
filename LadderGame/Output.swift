a240dfa5c15acd792fbe276b79581a21d052128b
도미닉 <kiljh@me.com>
2019-11-12 15:56:19 +0900

    출력하는 역할을 할 OutPut 구조체 생성

--- LadderGame/Output.swift
+++ LadderGame/Output.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct Output {
+    static func printLadders(height: Int, players: [LadderPlayer]) {
+        for _ in 0..<height {
+            print("|", terminator:"")
+            for _ in 1..<players.count {
+                if Int(arc4random_uniform(2))==1 {
+                    print("---", "|", separator:"", terminator:"")
+                }
+                else {
+                    print("   ", "|", separator:"", terminator:"")
+                }
+            }
+            print()
+        }
+    }
+}
